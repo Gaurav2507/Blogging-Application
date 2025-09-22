ceeffbca5f87f92ab8a15e8ba4ce8c4357dfd6a2
Gaurav Yadav <gaurav.settlemed@immabeme.in>
2025-09-23 00:47:11 +0530

    Added logout functionalooties

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,29 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Posts from "./pages/Posts";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  const navigate = useNavigate();
+  const user = localStorage.getItem("user");
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
   return (
     <div>
       <nav style={{ padding: "1rem", borderBottom: "1px solid #ddd" }}>
-        <Link to="/">Posts</Link> | <Link to="/login">Login</Link> |{" "}
-        <Link to="/register">Register</Link>
+        <Link to="/">Posts</Link> |{" "}
+        {!user ? (
+          <>
+            <Link to="/login">Login</Link> |{" "}
+            <Link to="/register">Register</Link>
+          </>
+        ) : (
+          <button onClick={handleLogout}>Logout</button>
+        )}
       </nav>
 
       <Routes>
